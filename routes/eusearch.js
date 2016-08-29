3b203904452b691c2a642ca0f7b36cb648a60299
Daniel Abzakh <daniel.abzakh@gmail.com>
2016-08-30 00:32:45 +0400

    Changes to EU search get request, check why query string adds up.

--- routes/eusearch.js
+++ routes/eusearch.js
@@ -1,14 +1,18 @@
 var http = require('http');
 var express = require('express');
 var router = express.Router();
+var querystring = require("querystring");
+
 var options = {
   hostname: 'data.theeuropeanlibrary.org',
-  path: '/opensearch/json?apikey=isuveu6roioo1b1ip6dub3ffml&query=romanov',
+  path: '/opensearch/json?apikey=isuveu6roioo1b1ip6dub3ffml&',
   method: 'get',
 };
 
 /* GET EU library search listing. */
 router.get('/eusearch', function(req, reseu, next) {
+  options.path = options.path + querystring.stringify(req.query);
+  console.log(options.path);
   var req = http.request(options, (res) => {
     res.setEncoding('utf8');
     res.on('data', (chunk) => {
