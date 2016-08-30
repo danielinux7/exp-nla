a9863d130605444c8077e6cfe1f469336ab3084d
Daniel Abzakh <daniel.abzakh@gmail.com>
2016-08-30 12:49:54 +0400

    Response not fixed yet

--- routes/eusearch.js
+++ routes/eusearch.js
@@ -6,20 +6,24 @@ var querystring = require("querystring");
 var options = {
   hostname: 'data.theeuropeanlibrary.org',
   path: '/opensearch/json?apikey=isuveu6roioo1b1ip6dub3ffml&',
-  method: 'get',
+  method: 'get'
 };
 
 /* GET EU library search listing. */
 router.get('/eusearch', function(req, reseu, next) {
   options.path = options.path + querystring.stringify(req.query);
-  console.log(options.path);
   var req = http.request(options, (res) => {
+    var body = '';
     res.setEncoding('utf8');
     res.on('data', (chunk) => {
+      body += chunk;
+    });
+    res.on('end', () => {
       if (!reseu.headersSent) {
-        reseu.send(chunk);
+        reseu.send(JSON.parse(body));
       }
     });
+
   });
   req.end();
 });
